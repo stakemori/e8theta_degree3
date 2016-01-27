41bb464f5d871b1aa0a0a725bb4065f47b1c05a2
Sho Takemori <stakemorii@gmail.com>
2016-01-27 15:53:27 +0900

    Add test for vectors

--- binding/main.c
+++ binding/main.c
@@ -1,5 +1,6 @@
 #include <fmpz.h>
-/* #include "e8theta.h" */
+#include <math.h>
+#include "e8theta.h"
 
 static inline int inner_prod(int s[8], int t[8]) {
   return ((2*s[0] + s[1] + s[2] + s[3] + s[4] + s[5] + s[6] + s[7]) * t[0] +
@@ -13,6 +14,7 @@ static inline int inner_prod(int s[8], int t[8]) {
 }
 
 
+
 static inline int norm(int s[8]) {
   return ((2*s[0] + s[1] + s[2] + s[3] + s[4] + s[5] + s[6] + s[7])*s[0] +
           (s[0] + 2*s[1] + s[2] + s[3] + s[4] + s[5] + s[6] + 2*s[7])*s[1] +
@@ -24,12 +26,80 @@ static inline int norm(int s[8]) {
           (s[0] + 2*s[1] + 2*s[2] + 2*s[3] + 2*s[4] + 2*s[5] + 2*s[6] + 4*s[7])*s[7]);
 }
 
-int main()
-{
-  int s[8] = {10, 3, 5, 3, 4, 5, -15, 10};
-  int a = inner_prod(s, s);
-  int b = norm(s);
-  printf("%d\n", a);
-  printf("%d\n", b);
+/* Fourier coefficients of Eisenstein series of weight 4 */
+int num_of_vectors[100] = {1, 240, 2160, 6720, 17520, 30240, 60480, 82560, 140400, 181680, 272160,
+                           319680, 490560, 527520, 743040, 846720, 1123440, 1179360, 1635120,
+                           1646400, 2207520, 2311680, 2877120, 2920320, 3931200, 3780240, 4747680, 4905600,
+                           6026880, 5853600, 7620480, 7150080, 8987760, 8951040, 10614240, 10402560, 13262640,
+                           12156960, 14817600, 14770560, 17690400, 16541280, 20805120, 19081920,
+                           23336640, 22891680, 26282880, 24917760, 31456320, 28318320, 34022160, 33022080,
+                           38508960, 35730720, 44150400, 40279680, 48297600, 46099200, 52682400, 49291200,
+                           61810560, 54475680, 64350720, 62497920, 71902320, 66467520, 80559360, 72183360, 86093280,
+                           81768960, 93623040, 85898880, 106282800, 93364320, 109412640, 105846720, 120187200,
+                           109969920, 132935040, 118329600, 141553440, 132451440, 148871520, 137229120, 168752640,
+                           148599360, 171737280, 163900800, 187012800, 169192800, 206025120, 181466880, 213183360,
+                           200202240, 224259840, 207446400, 251657280, 219041760,
+                           254864880, 241997760};
+
+int cached_vectors[max_diag_nm + 1][max_diag_size][8];
+
+int main() {
+  int cached_idx[max_diag_nm + 1] = {0};
+
+  double m = sqrt(2 * max_diag_nm);
+  int _s0_end = ceil(2 * m + 1);
+  for (int s0 = floor(-2*m); s0 < _s0_end; s0++)
+    {
+      double ds0 = s0;
+      int beg = floor(-m - ds0/2);
+      int end = ceil(m - ds0/2 + 1);
+      for (int s1 = beg; s1 < end; s1++)
+        {
+          for (int s2 = beg; s2 < end; s2++)
+            {
+              for (int s3 = beg; s3 < end; s3++)
+                {
+                  for (int s4 = beg; s4 < end; s4++)
+                    {
+                      for (int s5 = beg; s5 < end; s5++)
+                        {
+                          for (int s6 = beg; s6 < end; s6++)
+                            {
+                              double _centr = (2 * (s1 + s2 + s3 + s4 + s5 + s6) + ds0)/4;
+                              int _end = ceil(m/2 - _centr + 1);
+                              for (int s7 = floor(-m/2 - _centr); s7 < _end; s7++)
+                                {
+                                  int v[8] = {s0, s1, s2, s3, s4, s5, s6, s7};
+                                  int _nm = norm(v)/2;
+                                  if (_nm < max_diag_nm + 1)
+                                    {
+                                      int idx = ++cached_idx[_nm];
+                                      for (int i = 0; i < 8; i++)
+                                        {
+                                          cached_vectors[_nm][idx][i] = v[i];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+  int bl = 1;
+  for (int i = 0; i < max_diag_nm + 1; i++)
+    {
+      bl = bl && cached_idx[i] == num_of_vectors[i];
+    }
+  if (bl)
+    {
+        printf("true");
+    }
+  else
+    {
+      printf("false");
+    }
+
   return 0;
 }
