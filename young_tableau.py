2b2ab3089b0cb1f64ab93979c408636260993786
Sho Takemori <stakemorii@gmail.com>
2016-02-22 16:36:15 +0900

    Add a class for Young tableu

--- young_tableau.py
+++ young_tableau.py
@@ -2,9 +2,47 @@ from sage.all import PolynomialRing, QQ, matrix
 
 
 def schur_polynomial(n, wt):
-    R = PolynomialRing(QQ, names=["x%s" % i for i in range(n)])
-    l = [i + a for i, a in zip(reversed(range(n)), wt)]
+    R = PolynomialRing(QQ, names=["x%s" % i for i in xrange(n)])
+    l = [i + a for i, a in zip(reversed(xrange(n)), wt)]
     return R(_vandermonde(n, l) / _vandermonde(n, reversed(range(n))))
+
+
+class YoungTableu(object):
+
+    def __init__(self, n=None, numbers=None):
+        self._n = n
+        self._numbers = numbers
+
+    @property
+    def n(self):
+        return self._n
+
+    @property
+    def numbers(self):
+        return self._numbers
+
+    def weight(self):
+        return [len(l) for l in self.numbers]
+
+    def __repr__(self):
+        return "\n".join(str(a) for a in self.numbers)
+
+
+def _increasing_nums(n, m, lower_bds):
+    '''
+    n, m: positive integers,
+    lower_bds: list of integers of length m.
+    Returns a generator of m numbers (a0, a1, ..., a_(m-1)),
+    such that a0, a1, .. in [1, ..., n] and
+    a0 >= lower_bds[0], a1 >= lower_bds[1] .. and a_(m-1) >= lower_bds[m-1].
+    '''
+    pass
+
+
+def semistandard_young_tableaux(n, wt):
+    '''Returns a list of semistandard Young tableaux.
+    '''
+    pass
 
 
 def _vandermonde(n, wt):
