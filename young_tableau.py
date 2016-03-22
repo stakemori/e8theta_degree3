f0ca6ae7d52bd120a266e39f99518c8a75fa5c3c
Sho Takemori <stakemorii@gmail.com>
2016-03-22 16:54:53 +0900

    Add _index_of_gamma_0_gl_n

--- young_tableau.py
+++ young_tableau.py
@@ -1,5 +1,5 @@
-from sage.all import PolynomialRing, QQ, matrix, cached_function
-from itertools import combinations
+from sage.all import PolynomialRing, QQ, matrix, cached_function, ZZ, mul
+from itertools import combinations, groupby
 
 
 def poly_repn_dim(wt):
@@ -91,3 +91,24 @@ def semistandard_young_tableaux(n, wt):
 
     for a in _prod(col_lngs):
         yield YoungTableu(n=n, col_numbers=a)
+
+
+def _index_of_gamma_0_gl_n(alphas, p):
+    '''
+    Returns delta(a1, ..., an) defined in Shimura, Euler products and Eisenstein
+    series, pp 118, (15.1.7).
+    '''
+    if p in ZZ:
+        p = ZZ(p)
+
+    def _bn(n):
+        return mul(1 - p ** (-i) for i in xrange(1, n + 1))
+
+    e_r_ls = [(k, len(list(v)))
+              for k, v in groupby(sorted(alphas), lambda x: x)]
+    res = _bn(len(alphas)) / mul(_bn(r) for _, r in e_r_ls)
+    for i, (ei, ri) in enumerate(e_r_ls):
+        for j, (ej, rj) in enumerate(e_r_ls):
+            if i < j:
+                res *= p ** ((ej - ei) * ri * rj)
+    return res
