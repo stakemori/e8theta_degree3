421e5ce129b8d9461da862331e97c69f5ef5e5c9
Sho Takemori <stakemorii@gmail.com>
2016-04-12 13:54:34 +0900

    Declare _gl3_coset_gamma0

--- hecke_module.py
+++ hecke_module.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from sage.all import ZZ, mul
 from itertools import groupby
 
@@ -21,3 +23,24 @@ def _index_of_gamma_0_gl_n(alphas, p):
             if i < j:
                 res *= p ** ((ej - ei) * ri * rj)
     return res
+
+
+def _gl3_coset_gamma0(alphas, p):
+    r'''
+    Let alphas = [a0, a1, a2] with a0 <= a1 <= a2,
+    g = diag([p^a0, p^a1, p^a2]), and Gamma0 = g^(-1) GL3(Z) g ∧ GL3(Z).
+    Return a complete set Gamma0 \ GL3(Z).
+    '''
+    a0, a1, a2 = alphas
+    if a0 < a1 < a2:
+        return __gl3_coset_gamma0_distinct(a0, a1, a2, p)
+    elif a0 == a1 and a1 < a2:
+        pass
+    elif a0 < a1 and a1 == a2:
+        pass
+    else:
+        raise ValueError
+
+
+def __gl3_coset_gamma0_distinct(a0, a1, a2, p):
+    pass
