c0f73dda723a599bbbf2f889b2fc5a3c9dce80d8
Sho Takemori <stakemorii@gmail.com>
2016-04-15 16:39:43 +0900

    Add tests for cosets

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ FLINTOPT = -lm -lflint -lmpfr -lgmp -lpthread -I /usr/local/include/flint/
 OUT = binding/main
 TARGET = binding/main.c
 CC = gcc
+current_dir = $(shell pwd)
+parent_dir = $(shell dirname "$(current_dir)")
+
 compile:
 	$(CC) $(TARGET) -o $(OUT) $(FLINTOPT) -O2 -std=c11
 
@@ -10,5 +13,8 @@ debug:
 	$(CC) $(TARGET) -o $(OUT) $(DEBUGOPT) $(FLINTOPT)
 clean:
 	rm -f OUT
+
+test-coset:
+	sage -c 'sys.path.append("$(parent_dir)"); import e8theta_degree3.tests.hecke_module_test'
 
 .PHONY: compile clean debug
--- tests/hecke_module_test.py
+++ tests/hecke_module_test.py
@@ -1,6 +1,11 @@
 import unittest
 # from unittest import skip
 from ..hecke_module import _index_of_gamma_0_gl_n, _gl3_coset_gamma0
+from sage.rings.integer_ring import ZZ
+
+
+def alphas_ls():
+    return [[0, 1, 2], [0, 1, 3], [0, 2, 3], [0, 0, 1], [0, 0, 2], [0, 1, 1], [0, 2, 2]]
 
 
 class CosetTest(unittest.TestCase):
@@ -10,7 +15,39 @@ class CosetTest(unittest.TestCase):
                          _index_of_gamma_0_gl_n(alphas, p))
 
     def test_indices(self):
-        self.assert_index([0, 1, 2], 2)
+        for alphas in alphas_ls():
+            self.assert_index(alphas, 2)
+            self.assert_index(alphas, 3)
+
+    def check_distinct_cosets(self, alphas, p, gs):
+        res = True
+        p = ZZ(p)
+        a0, a1, a2 = alphas
+        for i, g in enumerate(gs):
+            for j, h in enumerate(gs):
+                if i < j:
+                    m = g * h**(-1)
+                    in_gam0 = ((ZZ(m[0, 1]) / p**(a1 - a0)) in ZZ and
+                               (ZZ(m[0, 2]) / p**(a2 - a0)) in ZZ and
+                               (ZZ(m[1, 2]) / p**(a2 - a1)) in ZZ)
+                    res = res and not in_gam0
+                    if not res:
+                        print g
+                        print h
+                        return False
+        return True
+
+    def test_distinct_cosets(self):
+        for p in [ZZ(2), ZZ(3)]:
+            for alphas in alphas_ls():
+                print alphas, p
+                gs = _gl3_coset_gamma0(alphas, p)
+                self.assertTrue(self.check_distinct_cosets(alphas, p, gs))
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(CosetTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
+
+# Local Variables:
+# compile-command: "cd ..; make test-coset"
+# End:
