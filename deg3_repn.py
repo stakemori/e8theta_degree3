c00055a0e3eeee4320f2dc4769414083e298c989
Sho Takemori <stakemorii@gmail.com>
2016-05-09 13:21:32 +0900

    Add a new module

---
+++ deg3_repn.py
@@ -0,0 +1,28 @@
+from sage.rings.all import PolynomialRing, QQ
+from sage.matrix.all import matrix
+from sage.misc.all import cached_function
+from .young_tablue import YoungTableau
+
+
+@cached_function
+def matrix_var(base_field=QQ):
+    R = PolynomialRing(base_field, names=[
+                       'x%s%s' % (i, j) for i in range(3) for j in range(3)])
+    return matrix(3, R.gens())
+
+
+def _bideterminant(a, b):
+    '''
+    a, b: an instance of young_tableau.YoungTableu
+    '''
+    m = matrix_var()
+    res = 1
+    for l1, l2 in zip(a.col_numbers, b.col_numbers):
+        res *= m.matrix_from_rows_and_columns(
+            [i - 1 for i in l1], [j - 1 for j in l2]).det()
+    return res
+
+
+def _t_lambda(wts):
+    return YoungTableau(n=3, col_numbers=[[i + 1 for _ in range(a)]
+                                          for i, a in enumerate(wts)])
