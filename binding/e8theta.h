605d2c773b59aba215d1813bd65ed39301e35cb0
Sho Takemori <stakemorii@gmail.com>
2016-06-23 16:06:37 +0900

    Delete e8theta.h

--- binding/e8theta.h
+++
@@ -1,7 +0,0 @@
-#ifndef e8thetaH
-#define e8thetaH
-
-#define MAX_NORM 5
-#define MAX_NM_OF_VECTORS 30240
-
-#endif
--- binding/e8vectors.h
+++ binding/e8vectors.h
@@ -1,7 +1,8 @@
 #ifndef _E8VECTORS_H_
 #define _E8VECTORS_H_
 
-#include "e8theta.h"
+#define MAX_NORM 5
+#define MAX_NM_OF_VECTORS 30240
 
 extern int num_of_vectors[100];
 
