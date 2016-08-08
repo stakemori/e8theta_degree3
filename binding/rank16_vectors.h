1bfb31134e669245e62e097983b9ea768583f1d7
Sho Takemori <stakemorii@gmail.com>
2016-08-08 16:22:48 +0900

    Remove static and change names

--- binding/rank16_vectors.h
+++ binding/rank16_vectors.h
@@ -12,4 +12,10 @@ Rk16VecInt cached_vectors_rk16[MAX_NORM_RK16 + 1][MAX_NM_OF_VECTORS_RK16][16];
 
 void cache_vectors_rk16(void);
 
+/* Extern followings only for tests */
+void _convert_to_euclid_vector_rk16(Rk16VecInt vec[16]);
+void _convert_from_euclid_vector_rk16(Rk16VecInt vec[16]);
+void normalize_vec_rk16(Rk16VecInt vec[16]);
+int repr_modulo_autom_rk16(int n, int repr[MAX_NM_OF_VECTORS_RK16][17]);
+
 #endif /* _RANK16_VECTORS_H_ */
