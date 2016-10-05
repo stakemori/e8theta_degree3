f4d08875be7b165bd71c02eb0c82d135fdc97d2e
Sho Takemori <stakemorii@gmail.com>
2016-10-05 15:24:50 +0900

    Change the order of FC

--- results/data/data_utils.py
+++ results/data/data_utils.py
@@ -6,3 +6,15 @@ from sage.misc.all import cached_function
 @cached_function
 def data_dir():
     return join(dirname(e8theta_degree3.__file__), "results", "data")
+
+
+def half_int_mat_to_list(t):
+    return ([t.T[a, a] for a in range(3)] +
+            [2 * t.T[i, j] for i, j in [(1, 2), (0, 2), (0, 1)]])
+
+
+def sort_ts(ts):
+    def key(t):
+        T = t.T
+        return (T.det(), T[0, 0], T[1, 1], T[2, 2], T[0, 1], T[0, 2], T[1, 2])
+    return list(sorted(ts, key=key))
